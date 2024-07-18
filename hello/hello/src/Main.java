87424d60390b2df3c71407e8385574e9b701a7b0
Madina <usmonqulovamadina9@gmail.com>
2024-07-18 22:18:26 +0500

    added likeService and like mode

--- hello/hello/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("salom gruppa : Madina");
-        System.out.println("salom gruppa : Jamol");
-    }
-}
